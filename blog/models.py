b05beb3ebd80792c7b23208795f9bc1ea5104191
Victoria-Hodder <morethangrammar@gmail.com>
2019-11-02 11:31:57 +0100

    added features to allow user to create, update and delete posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -10,3 +11,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+        # this finds the location of a specific post
+        # reverse = return the full url to that route as a string
+        # lets the view handle the redirect for you
